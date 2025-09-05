a2cff6fe269b082c6dc970fe53a4f521b6139ef9
ptv1201 <phamthuongvi1201@gmail.com>
2025-09-05 19:07:16 +0700

    5/9

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
 try {
-     await mongoose.connect('mongodb://127.0.0.1:27017/blog_dev')
+     await mongoose.connect('mongodb+srv://ptv1201:hpvbo1201@nodejsf8.r9cyai2.mongodb.net/?retryWrites=true&w=majority&appName=NodejsF8')
      console.log('connect successfully')
 } catch (error) {
     console.log('connect faile')
